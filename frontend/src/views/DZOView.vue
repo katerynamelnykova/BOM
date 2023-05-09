a9fbb0bffd6ce7cb663ebfed0a4c80edd11d5c85
Kateryna Melnykova <kate.meln2803@gmail.com>
2023-05-08 20:36:03 -0700

    dzo and to view

--- frontend/src/views/DZOView.vue
+++ frontend/src/views/DZOView.vue
@@ -1,6 +1,34 @@
 <template>
-  <div class="home">
-    {{ $store.state.dzos }}
+  <div class="dzo">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto text-left mt-4">
+                <p class="h2">Деталі або зборочні одиниці:</p>
+                <table class="table table-sm table-dark mt-4">
+                <thead>
+                    <tr>
+                    <th scope="col">idDetail</th>
+                    <th scope="col">idParentDetail</th>
+                    <th scope="col">cost</th>
+                    <th scope="col">weight</th>
+                    <th scope="col">name</th>
+                    <th scope="col">isManufactured</th>
+                    </tr>
+                </thead>
+                <tbody>  
+                    <tr :key="dzo.idDetail" v-for="dzo in $store.state.dzos">
+                        <th scope="row">{{ dzo.idDetail }}</th>
+                        <td>{{ dzo.idParentDetail }}</td>
+                        <td>{{ dzo.cost }}</td>
+                        <td>{{ dzo.weight }}</td>
+                        <td>{{ dzo.name }}</td>
+                        <td>{{ dzo.isManufactured }}</td>
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
--- frontend/src/views/TOView.vue
+++ frontend/src/views/TOView.vue
@@ -1,6 +1,42 @@
 <template>
-  <div class="home">
-    {{ $store.state.tos }}
+  <div class="to">
+    <div class="container">
+        <div class="row">
+            <div class="col-ml-8 mx-auto text-left mt-4">
+                <p class="h2">Технологічні операції:</p>
+                <table class="table table-sm table-dark mt-4">
+                <thead>
+                    <tr>
+                    <th scope="col">idTO</th>
+                    <th scope="col">idManufacturedDetail</th>
+                    <th scope="col">sequenceNumber</th>
+                    <th scope="col">duration</th>
+                    <th scope="col">district</th>
+                    <th scope="col">department</th>
+                    <th scope="col">equipmentTimeCost</th>
+                    <th scope="col">workerTimeCost</th>
+                    <th scope="col">cost</th>
+                    <th scope="col">name</th>
+                    </tr>
+                </thead>
+                <tbody>  
+                    <tr :key="to.idTO" v-for="to in $store.state.tos">
+                        <th scope="row">{{ to.idTO }}</th>
+                        <td>{{ to.idManufacturedDetail }}</td>
+                        <td>{{ to.sequenceNumber }}</td>
+                        <td>{{ to.duration }}</td>
+                        <td>{{ to.district }}</td>
+                        <td>{{ to.department }}</td>
+                        <td>{{ to.equipmentTimeCost }}</td>
+                        <td>{{ to.workerTimeCost }}</td>
+                        <td>{{ to.cost }}</td>
+                        <td>{{ to.name }}</td>
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
